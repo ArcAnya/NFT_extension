eea4a4f1e36a4a21bb62123c6b555a8219894d39
Arc_Anya <arcanyatwitch@gmail.com>
2021-04-13 11:40:10 +0200

    first chai test running

--- test/VideoNFT.test.js
+++ test/VideoNFT.test.js
@@ -1,14 +1,24 @@
+const { assert } = require('chai')
+
 const VideoNFT = artifacts.require('./VideoNFT.sol')
 
-require('chai')
-  .use(require('chai-as-promised'))
-  .should()
-
 contract('VideoNFT', (accounts) => {
-    describe('deployment', async () => {
-      // async needed because interaction with blockchain
-      it('deploys successfully', async () => {
-        
-      })
+
+  let contract // declaring variable for contract instance
+
+  before(async () => {
+    contract = await VideoNFT.deployed()
+  })
+
+  describe('deployment', async () => {
+    // async needed because interaction with blockchain
+    it('deploys successfully', async () => {
+      // checking that the contract has an address not null
+      const address = contract.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
     })
+  })
 })
